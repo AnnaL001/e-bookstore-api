51dc54b6aa74415af51800707af6303d87cd09d4
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-11-15 14:54:59 +0300

    :tada: Add initial setup

---
+++ src/test/java/com/anna/e_bookstore/EBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anna.e_bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
