54d04da6dddd566bb557d3452178659fbe248b03
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-11-15 23:33:24 +0300

    :label: Add genre entity

---
+++ src/main/java/com/anna/e_bookstore/model/Genre.java
@@ -0,0 +1,44 @@
+package com.anna.e_bookstore.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "genres")
+public class Genre {
+  @Id
+  @GeneratedValue
+  private Long id;
+  private String title;
+  private String description;
+
+  public Genre() {
+  }
+
+  public Genre(String title, String description) {
+    this.title = title;
+    this.description = description;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
