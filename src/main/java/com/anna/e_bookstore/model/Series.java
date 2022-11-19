eaac528b4e52b00b8650d73079b90d4d683bd661
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-11-20 00:28:48 +0300

    :label: Update series entity

--- src/main/java/com/anna/e_bookstore/model/Series.java
+++ src/main/java/com/anna/e_bookstore/model/Series.java
@@ -1,7 +1,6 @@
 package com.anna.e_bookstore.model;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
 @Table(name = "series")
@@ -10,12 +9,6 @@ public class Series {
   @GeneratedValue
   private Long id;
   private String title;
-
-  @OneToOne(mappedBy = "series")
-  private BookSeries bookSeries;
-
-  @OneToMany(mappedBy = "series")
-  List<AuthorSeries> series;
 
   public Series() {
   }
@@ -34,21 +27,5 @@ public class Series {
 
   public void setTitle(String title) {
     this.title = title;
-  }
-
-  public BookSeries getBookSeries() {
-    return bookSeries;
-  }
-
-  public void setBookSeries(BookSeries bookSeries) {
-    this.bookSeries = bookSeries;
-  }
-
-  public List<AuthorSeries> getSeries() {
-    return series;
-  }
-
-  public void setSeries(List<AuthorSeries> series) {
-    this.series = series;
   }
 }
