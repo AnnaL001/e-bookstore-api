9452cca153f6a9f7607752782ab2477b124fd2b3
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-11-21 12:25:55 +0300

    :label: Update filetype entity

--- src/main/java/com/anna/e_bookstore/model/FileType.java
+++ src/main/java/com/anna/e_bookstore/model/FileType.java
@@ -1,15 +1,12 @@
 package com.anna.e_bookstore.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "file_types")
 public class FileType {
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
 
