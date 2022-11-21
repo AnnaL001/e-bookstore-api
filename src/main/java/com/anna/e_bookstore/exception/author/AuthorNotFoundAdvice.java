456733827a0ce206ba333e1f0d25d4d2dd1ada8b
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-11-22 00:09:29 +0300

    :art: Update author resource exception handling

--- src/main/java/com/anna/e_bookstore/exception/author/AuthorNotFoundAdvice.java
+++ src/main/java/com/anna/e_bookstore/exception/author/AuthorNotFoundAdvice.java
@@ -1,6 +1,8 @@
 package com.anna.e_bookstore.exception.author;
 
+import com.anna.e_bookstore.controller.AuthorController;
 import net.minidev.json.JSONObject;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,15 +12,19 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @ControllerAdvice
 public class AuthorNotFoundAdvice {
   @ResponseBody
   @ExceptionHandler(AuthorNotFoundException.class)
   @ResponseStatus(HttpStatus.NOT_FOUND)
-  JSONObject authorNotFoundHandler(AuthorNotFoundException exception){
+  EntityModel<JSONObject> authorNotFoundHandler(AuthorNotFoundException exception){
     Map<String, String> message = new HashMap<>();
     message.put("type", "ERROR");
     message.put("message", exception.getMessage());
-    return new JSONObject(message);
+    return EntityModel.of(new JSONObject(message),
+            linkTo(methodOn(AuthorController.class).getAll()).withRel("authors"));
   }
 }
