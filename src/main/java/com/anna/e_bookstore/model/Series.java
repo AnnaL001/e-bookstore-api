a79766c841819726919d67e1d26a5118560d579f
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-11-21 12:26:53 +0300

    :label: Update series entity

--- src/main/java/com/anna/e_bookstore/model/Series.java
+++ src/main/java/com/anna/e_bookstore/model/Series.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 @Table(name = "series")
 public class Series {
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String title;
 
