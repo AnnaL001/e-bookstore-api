81fd8eea40eca7390f00fc36052a3373aa076cf6
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-11-21 12:31:30 +0300

    :sparkles: Add endpoint to create author resource

--- src/main/java/com/anna/e_bookstore/controller/AuthorController.java
+++ src/main/java/com/anna/e_bookstore/controller/AuthorController.java
@@ -4,12 +4,13 @@ import com.anna.e_bookstore.assembler.AuthorModelAssembler;
 import com.anna.e_bookstore.exception.author.AuthorNotFoundException;
 import com.anna.e_bookstore.model.Author;
 import com.anna.e_bookstore.service.author.AuthorService;
+import com.anna.e_bookstore.service.socials.SocialService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,11 +21,13 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RestController
 public class AuthorController {
   private final AuthorService authorService;
+  private final SocialService socialService;
   private final AuthorModelAssembler authorModelAssembler;
 
   @Autowired
-  public AuthorController(AuthorService authorService, AuthorModelAssembler authorModelAssembler){
+  public AuthorController(AuthorService authorService, SocialService socialService, AuthorModelAssembler authorModelAssembler){
     this.authorService = authorService;
+    this.socialService = socialService;
     this.authorModelAssembler = authorModelAssembler;
   }
 
@@ -47,5 +50,15 @@ public class AuthorController {
     }
 
     return authorModelAssembler.toModel(author);
+  }
+
+  @PostMapping("/authors")
+  public ResponseEntity<?> add(@RequestBody Author author){
+    socialService.add(author.getSocials());
+    authorService.add(author);
+    EntityModel<Author> entityModel = authorModelAssembler.toModel(authorService.get(author.getId()));
+    return ResponseEntity
+            .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+            .body(entityModel);
   }
 }
