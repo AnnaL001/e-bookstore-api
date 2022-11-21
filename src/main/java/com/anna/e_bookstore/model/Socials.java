fda450483d6f661be2c9300a3481c5d3b54483a7
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-11-21 12:27:09 +0300

    :label: Update socials entity

--- src/main/java/com/anna/e_bookstore/model/Socials.java
+++ src/main/java/com/anna/e_bookstore/model/Socials.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 @Table(name = "socials")
 public class Socials {
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String instagram;
   private String facebook;
