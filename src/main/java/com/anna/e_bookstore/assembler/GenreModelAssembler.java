97c20b9353920bcaa8346a5ed27e952cff200ff2
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-11-25 09:17:56 +0300

    :art: Update model assembler

--- src/main/java/com/anna/e_bookstore/assembler/GenreModelAssembler.java
+++ src/main/java/com/anna/e_bookstore/assembler/GenreModelAssembler.java
@@ -1,5 +1,6 @@
 package com.anna.e_bookstore.assembler;
 
+import com.anna.e_bookstore.controller.BookController;
 import com.anna.e_bookstore.controller.GenreController;
 import com.anna.e_bookstore.model.Genre;
 import org.springframework.hateoas.EntityModel;
@@ -15,6 +16,7 @@ public class GenreModelAssembler implements RepresentationModelAssembler<Genre, 
   public EntityModel<Genre> toModel(Genre genre) {
     return EntityModel.of(genre,
             linkTo(methodOn(GenreController.class).get(genre.getId())).withSelfRel(),
+            linkTo(methodOn(BookController.class).getGenreBooks(genre.getId())).withRel("genre_books"),
             linkTo(methodOn(GenreController.class).getAll()).withRel("genres"));
   }
 }
